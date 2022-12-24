44226e8ff7ea011b1dc4d4ddf3a61d1a4660be83
alecyan1993 <alec.yan1993@gmail.com>
2022-12-24 15:49:58 +0800

    [UPD]: finished marker feature points process

--- marker.py
+++ marker.py
@@ -10,10 +10,19 @@ import torch
 import cv2
 import yaml
 import numpy as np
+from scipy.spatial.transform import Rotation as R
+
+
+def from_Rt2T(rotation_matrix, translation_vector):
+    translation_vector = np.array(translation_vector).reshape(3, 1)
+    T = np.concatenate((rotation_matrix, translation_vector), axis=1)
+    T = np.concatenate((T, np.array([[0, 0, 0, 1]])), axis=0)
+    return T
 
 
 class Marker:
     def __init__(self, config):
+        self.features = None
         self.device = 'cuda:0' if torch.cuda.is_available() else 'cpu:0'
         with open(config, 'r') as f:
             self.config = yaml.safe_load(f)
@@ -26,10 +35,28 @@ class Marker:
         marker_tensor = torch.from_numpy(self.marker / 255.0).float()[None, None].to(self.device)
         sp_res = self.sp({'image': marker_tensor})
         sp_res['image_size'] = torch.tensor(marker_tensor.shape)
+        kp_3d = self.convert_2d_3d(sp_res)
+        sp_res['keypoints_3d'] = kp_3d
         self.features = sp_res
-        if self.config['save']:
-            np.save(self.config['feature_path'], self.features)
         return sp_res
+
+    def convert_2d_3d(self, sp_res):
+        kp_3d = []
+        r = R.from_euler('xyz', [self.config['rotation']['x'] / 180 * np.pi,
+                                 self.config['rotation']['y'] / 180 * np.pi,
+                                 self.config['rotation']['z'] / 180 * np.pi])
+        rot_mat = r.as_matrix()
+        translation = [self.config['position']['x'],
+                       self.config['position']['y'],
+                       self.config['position']['z']]
+        T = from_Rt2T(rot_mat, translation)
+        for kp in sp_res['keypoints'][0].cpu().detach().numpy().astype(int):
+            trans = [kp[0] * self.config['size']['length'] / self.marker.shape[1],
+                     kp[1] * self.config['size']['length'] / self.marker.shape[1],
+                     0, 1]
+            t = T @ np.array(trans)
+            kp_3d.append(list(t)[:3])
+        return np.array(kp_3d)
 
     def visualize_features(self):
         kps = self.features['keypoints'][0].cpu().detach().numpy().astype(int)
