52bf6c7b1e8e612c27db9edc39b2f0f896c8fb83
alecyan1993 <alec.yan1993@gmail.com>
2022-12-26 13:15:09 +0800

    [UPD]: generated pixel points and 3d points for the query images

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 from util.model import superpoint, superglue
 from util.feature_matcher import FeatureMatcher
+from util.pnp_solver import PnpSolver
 from query import QueryImage
 import torch
 import cv2
 import yaml
 import numpy as np
 from scipy.spatial.transform import Rotation as R
+from marker import Marker
 
 
 def get_marker(fp):
@@ -41,6 +43,21 @@ def visualize_superglue(sg_res, query_img, marker_img, match_threshold=0.3):
     cv2.waitKey(0)
 
 
+def get_pairs(sg_res, origin_features, match_threshold=0.6):
+    kpts0 = sg_res['keypoints0'].squeeze().cpu().detach().numpy()
+    kpts1 = sg_res['keypoints1'].squeeze().cpu().detach().numpy()
+    matches0 = sg_res['matches0'].squeeze().cpu().detach().numpy()
+    matches1 = sg_res['matches1'].squeeze().cpu().detach().numpy()
+    matching_scores0 = sg_res['matching_scores0'].squeeze().cpu().detach().numpy()
+    kpts1_3d = origin_features['keypoints_3d']
+
+    pointsid1 = matches0[matching_scores0 > match_threshold]
+    kpts0_filter = [list(kpts0[matches1[i]].astype(int)) for i in pointsid1]
+    kpts1_filter = [kpts1[i] for i in pointsid1]
+    kpts1_3d_filter = [list(kpts1_3d[i]) for i in pointsid1]
+    return kpts0_filter, kpts1_filter, kpts1_3d_filter
+
+
 if __name__ == '__main__':
     device = 'gpu:0' if torch.cuda.is_available() else 'cpu:0'
     sp = superpoint.SuperPoint({}).eval().to(device)
@@ -59,7 +76,14 @@ if __name__ == '__main__':
     query_features = qi.get_feature(sp)
 
     match = feature_matcher.match_superglue(query_features, marker_features)
-    visualize_superglue(match, img, marker, match_threshold=0.6)
+    # visualize_superglue(match, img, marker, match_threshold=0.6)
+    kp_query_pixel, kp_marker_pixel, kp_query_3d = get_pairs(match, marker_features, match_threshold=0.6)
+
+    # Pnp Solver
+    pnp_solver = PnpSolver(min_points3d=50)
+    pose_guess = {'position': [10, 10, 2],
+                  'quaternion': [-0.3154602271415246, -0.40056517567031236, 0.6800333831230863, -0.5268652417556118]}
+    pnp_pose, flag = pnp_solver.solve_pnp(kp_query_pixel, kp_query_3d, query['camera_intrinsic'], pose_guess)
 
 
 
